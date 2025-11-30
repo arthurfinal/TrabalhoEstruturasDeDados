0a16fb88b26370f3ebb5f7aa5b5fc7e791b3563e
arthurfinal <arthurfelipe123476@gmail.com>
2025-11-30 18:01:32 -0300

    Teste: Busca Sequencial

---
+++ src/testes/TesteBusca.java
@@ -0,0 +1,26 @@
+package testes;
+
+import estruturas.ArvoreBST;
+import estruturas.ArvoreAVL;
+import busca.BuscaSequencial;
+import busca.BuscaBinaria;
+import utils.Cronometro;
+
+public class TesteBusca {
+
+    private static final int REPETICOES = 5;
+
+    public static double testarBuscaSequencial(int[] dados, int valor) {
+        Cronometro cronometro = new Cronometro();
+        double tempoTotal = 0;
+
+        for (int r = 0; r < REPETICOES; r++) {
+            cronometro.iniciar();
+            BuscaSequencial.buscar(dados, valor);
+            cronometro.parar();
+            tempoTotal += cronometro.getTempoEmMilissegundos();
+        }
+
+        return tempoTotal / REPETICOES;
+    }
+}
