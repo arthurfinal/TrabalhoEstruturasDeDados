bde9db137fb62c684e9cc726d8c8a655ec6f05a5
arthurfinal <arthurfelipe123476@gmail.com>
2025-11-30 12:34:16 -0300

    Busca Binaria

--- src/busca/BuscaBinaria.java
+++ src/busca/BuscaBinaria.java
@@ -1,1 +1,23 @@
+package busca;
 
+public class BuscaBinaria {
+    public static boolean buscar(int[] arr, int valor) {
+        int esquerda = 0;
+        int direita = arr.length -1;
+
+        while (esquerda <= direita) {
+            int meio = esquerda + (direita - esquerda) / 2;
+
+            if (arr[meio] == valor) {
+                return true;
+            }
+
+            if (arr[meio] < valor) {
+                esquerda = meio + 1;
+            }else{
+                direita = meio - 1;
+            }
+        }
+        return false;
+    } 
+}
