45717970b12f2ed4c8ef06246f033416b9f51bab
arthurfinal <arthurfelipe123476@gmail.com>
2025-11-30 14:21:36 -0300

    Estrutura Arvore BST: Inserir Recursivo

--- src/estruturas/ArvoreBST.java
+++ src/estruturas/ArvoreBST.java
@@ -1,1 +1,35 @@
+package estruturas;
+
+public class ArvoreBST {
+    private No raiz;
+
+    private class No {
+        int valor;
+        No esquerda;
+        No direita;
+
+        public No(int valor) {
+            this.valor = valor;
+            this.esquerda = null;
+            this.direita = null;
+        }
+    }
+
+    public void inserir(int valor) {
+        raiz = inserirRecursivo(raiz, valor);
+    }
+
+    private No inserirRecursivo(No atual, int valor) {
+        if (atual == null) {
+            return new No(valor);
+        }
+
+        if (valor < atual.valor) {
+            atual.esquerda = inserirRecursivo(atual.esquerda, valor);
+        } else if (valor > atual.valor) {
+            atual.direita = inserirRecursivo(atual.direita, valor);
+        }
+        return atual;
+    }
+}
 
