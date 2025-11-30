9befef7441e71e5b4ae974e852dfcd79e873aa96
arthurfinal <arthurfelipe123476@gmail.com>
2025-11-30 16:11:06 -0300

    Utils: Cronometro

--- src/utils/Cronometro.java
+++ src/utils/Cronometro.java
@@ -1,1 +1,19 @@
+package utils;
+
+public class Cronometro {
+    private long inicio;
+    private long fim;
+
+    public void iniciar() {
+        this.inicio = System.nanoTime();
+    }
+
+    public void parar() {
+        this.fim = System.nanoTime();
+    }
+
+    public double getTempoEmMilissegundos() {
+        return (double) (fim - inicio) / 1_000_000.0;
+    }
+}
 
