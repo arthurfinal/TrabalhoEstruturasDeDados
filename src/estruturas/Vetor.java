793a7b45148509fbe647c4521db459f67ec87df6
arthurfinal <arthurfelipe123476@gmail.com>
2025-11-30 14:37:21 -0300

    Estrutura Arvores: Vetor: inserir

--- src/estruturas/Vetor.java
+++ src/estruturas/Vetor.java
@@ -1,1 +1,21 @@
+package estruturas;
+
+public class Vetor {
+    private int[] elementos;
+    private int tamanhoAtual;
+
+    public Vetor(int capacidade) {
+        this.elementos = new int[capacidade];
+        this.tamanhoAtual = 0;
+    }
+
+    public void inserir(int valor) {
+        if (tamanhoAtual < elementos.length) {
+            elementos[tamanhoAtual] = valor;
+            tamanhoAtual++;
+        } else {
+            System.err.println("Vetor cheio. Não foi possível inserir " + valor);
+        }
+    }
+}
 
