10d909b7fa0d8959d4c51df5dd132124b3ac7cee
arthurfinal <arthurfelipe123476@gmail.com>
2025-11-30 19:36:58 -0300

    Teste: Ordenção: Bubble  e Merge Sort

--- src/testes/TesteOrdenacao.java
+++ src/testes/TesteOrdenacao.java
@@ -1,1 +1,40 @@
+package testes;
 
+import ordenacao.BubbleSort;
+import ordenacao.MergeSort;
+import utils.Cronometro;
+
+public class TesteOrdenacao {
+
+    private static final int REPETICOES = 5;
+
+    public static double testarBubbleSort(int[] dados) {
+        Cronometro cronometro = new Cronometro();
+        double tempoTotal = 0;
+
+        for (int r = 0; r < REPETICOES; r++) {
+            int[] copia = dados.clone();
+            cronometro.iniciar();
+            BubbleSort.ordenar(copia);
+            cronometro.parar();
+            tempoTotal += cronometro.getTempoEmMilissegundos();
+        }
+
+        return tempoTotal / REPETICOES;
+    }
+
+    public static double testarMergeSort(int[] dados) {
+        Cronometro cronometro = new Cronometro();
+        double tempoTotal = 0;
+
+        for (int r = 0; r < REPETICOES; r++) {
+            int[] copia = dados.clone();
+            cronometro.iniciar();
+            MergeSort.ordenar(copia);
+            cronometro.parar();
+            tempoTotal += cronometro.getTempoEmMilissegundos();
+        }
+
+        return tempoTotal / REPETICOES;
+    }
+}
